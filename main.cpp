8a7c79c3ee2559d15a27b46dadc18fd663c35d68
Fizzzn <btordz6@gmail.com>
2025-10-19 18:35:49 +0000

    Linkedlist

--- main.cpp
+++ main.cpp
@@ -1,103 +1,103 @@
-
 #include <iostream>
-#include <list>
 using namespace std;
 
 struct Node{
     int data;
     Node *next;
-    
-    Node(int d){ 
+
+    Node(int d){
         data = d;
         next = nullptr;
     }
 };
 
 class LinkedList{
-    private: Node* head;
-    
-    public:
+private:
+    Node* head;
+
+public:
     LinkedList(): head(nullptr){}
-    // Function to append a new node at the end
+
+    // Append a new node at the end
     void Append(int val) {
         Node* newNode = new Node(val);
-        if (!head) {        
-            //**** Please add your code here 
+        if (!head) {
+            head = newNode;                 // seting first node
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) temp = temp->next; // now go to last
+        temp->next = newNode;                 // linking at tail
+    }
 
+    // now inserting a node at the front (the head)
+    void InsertNodeToHead(int val){
+        Node *newNode = new Node(val);
+        newNode->next = head;
+        head = newNode; //updating head
+}
 
-           //**** End of adding your code 
+    // Here lets insert a node after the first node (value == key)
+    void InsertNode(int key, int val){
+        Node* temp = head;
+        // find our key
+        while (temp && temp->data != key) temp = temp->next;
+
+        if(!temp){
+            cout << "Node with value " << key << " not found!" << endl;
+            return;
+        }
+        // insert after temp node
+        Node* newNode = new Node(val);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    // insert a tails node at the end of list
+    void InsertNodeToTail(int val){
+        Node *newNode = new Node(val);
+
+        if (!head) {
+         cout << "The list is empty." << endl; 
+            head = newNode;
             return;
         }
 
-        Node* temp = head;
-        //**** Please add your code here 
-
-
-        //**** End of adding your code 
-    }
-    
-    //Insert a node at the front of a linked list
-    void InsertNodeToHead(int val){
-        Node *newNode = new Node{val};
-        //**** Please add your code here 
-
-
-        //**** End of adding your code 
-    }
-    
-    //Insert a node after a given node with the value "key".
-    void InsertNode(int key, int val){
-        Node* temp = head;
-		//find the node with the value "key"
-        //**** Please add your code here 
-
-
-        //**** End of adding your code 
-
-        if(!temp){
-            cout << "Node with value " << key << "not found!" << endl;
-            return; 
-        }
-		//Insert the new node after the node with the value "key"
-        //**** Please add your code here 
-
-
-        //**** End of adding your code 
-    }
-    
-    //Insert a node at the end of a linked list
-    void InsertNodeToTail(int val){
-        Node *newNode = new Node{val};
-        
-        if (!head) { //The list is empty
-            cout << "The list is empty." << endl;
-            //**** Please add your code here 
-
-
-            //**** End of adding your code 
-        }
         Node* last = head;
-		// Traverse to the last node
-        //**** Please add your code here 
-
-
-        //**** End of adding your code 
+        while (last->next) last = last->next;
+        last->next = newNode;
     }
-    
+
+    // anything with a vallue of d must be deleted
     void DeleteNode(int d){
+        if (!head) {
+            cout << "Node with value " << d << " not found!" << endl;
+            return;
+        }
+
+        // delete head
+        if (head->data == d) {
+            Node* toDel = head;
+            head = head->next;
+            delete toDel;
+            return;
+        }
+
+        // lets find the predecessor of node to delete
         Node *temp = head;
-		// find the node with the value "d"
-		//**** Please add your code here 
+        while (temp->next && temp->next->data != d) temp = temp->next;
 
+        if (temp->next == nullptr) {
+            cout << "Node with value " << d << " not found!" << endl;
+            return;
+        }
 
-        //**** End of adding your code 
-
-		if (temp->next == nullptr) {
-			cout << "Node with value " << d << " not found!" << endl;
-		}
+        Node* toDel = temp->next;
+        temp->next = toDel->next;
+        delete toDel;
     }
-    
-     // Function to display the linked list
+
+    // Display
     void Display() {
         Node* temp = head;
         while (temp) {
@@ -106,8 +106,8 @@ class LinkedList{
         }
         cout << "NULL" << endl;
     }
-    
-    // Destructor to free memory
+
+    // Destructor
     ~LinkedList() {
         Node* temp;
         while (head) {
@@ -117,7 +117,6 @@ class LinkedList{
         }
     }
 };
-
 
 int main()
 {
@@ -132,31 +131,26 @@ int main()
     cout << "" << endl;
 
     int listArray[] = {1, 3, 5, 7, 9};
-    
-    Node *head = nullptr;
-    
+
     LinkedList lkList;
-    
-    for(int i = 0; i < sizeof(listArray)/sizeof(listArray[0]); i++)
-    {
-        lkList.Append(listArray[i]);
-    }
-    
-    cout<<"The created linked List: ";
+
+    for (int x : listArray) lkList.Append(x);
+
+    cout << "The created linked List: ";
     lkList.Display();
-	cout << "" << endl;
-    
+    cout << "" << endl;
+
     lkList.InsertNodeToHead(0);
-    cout<<"The linked list after adding 0: ";
+    cout << "The linked list after adding 0: ";
     lkList.Display();
     cout << "" << endl;
 
     lkList.InsertNode(5, 6);
-    cout<<"The linked list after adding 6: ";
+    cout << "The linked list after adding 6: ";
     lkList.Display();
     cout << "" << endl;
 
-	lkList.DeleteNode(0);
+    lkList.DeleteNode(0);
     cout << "The linked list after deleting 0: ";
     lkList.Display();
     cout << "" << endl;
